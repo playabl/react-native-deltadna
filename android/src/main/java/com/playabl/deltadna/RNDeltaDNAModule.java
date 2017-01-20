09bb4ac65835fc820610db31175e50c4952afc35
Johannes Stein <johannesstein@freeze-dev.com>
2017-01-20 21:23:25 +0000

    :bug: Fix reference to Promise

--- android/src/main/java/com/playabl/deltadna/RNDeltaDNAModule.java
+++ android/src/main/java/com/playabl/deltadna/RNDeltaDNAModule.java
@@ -23,6 +23,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 
 public class RNDeltaDNAModule extends ReactContextBaseJavaModule {
 
